a900d64fc676d65d1c29754d64f809de3ef17304
Dinodup <dudup2626@gmail.com>
2024-08-27 09:13:38 -0300

    comita

--- Lista 3/at5.php
+++ Lista 3/at5.php
@@ -4,7 +4,36 @@
     $mes = substr($data,5,2);
     $ano = substr($data,0,4);
     
-    echo "Dia: " . $dia . "<br>";
-    echo "Mês: " . $mes . "<br>";
-    echo "Ano: " . $ano . "<br>";
+    $data1 = date_create($data);
+
+    $dia_semana = date_format($data1, 'w');
+
+    switch ($dia_semana) {
+        case 0:
+            $dia_semana = 'Domingo';
+            break;
+        case 1:
+            $dia_semana = 'Segunda-feira';
+            break;
+        case 2:
+            $dia_semana = 'Terça-feira';
+            break;
+        case 3:
+            $dia_semana = 'Quarta-feira';
+            break;
+        case 4:
+            $dia_semana = 'Quinta-feira';
+            break;
+        case 5:
+            $dia_semana = 'Sexta-feira';
+            break;
+        case 6:
+            $dia_semana = 'Sábado';
+            break;
+        default:
+            $dia_semana = 'Dia inválido';
+            break;
+    }
+
+    echo "O dia da semana é: " . $dia_semana;
 ?>
